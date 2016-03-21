85c55b9bdcd0ccd6e235952326343e678730efa3
Viktor <xu.redzonez@gmail.com>
2016-03-21 10:45:14 +0700

    Ketuk Pintu Jakarta

---
+++ merger data.sql
@@ -0,0 +1,100 @@
+REPLACE INTO epus_prog_3172.app_users_list		select * from epus_prog_3172010.app_users_list;
+REPLACE INTO epus_prog_3172.app_users_profile	select * from epus_prog_3172010.app_users_profile;
+
+REPLACE INTO epus_prog_3172.app_users_list		select * from epus_prog_3172020.app_users_list;
+REPLACE INTO epus_prog_3172.app_users_profile	select * from epus_prog_3172020.app_users_profile;
+
+REPLACE INTO epus_prog_3172.app_users_list		select * from epus_prog_3172030.app_users_list;
+REPLACE INTO epus_prog_3172.app_users_profile	select * from epus_prog_3172030.app_users_profile;
+
+REPLACE INTO epus_prog_3172.app_users_list		select * from epus_prog_3172040.app_users_list;
+REPLACE INTO epus_prog_3172.app_users_profile	select * from epus_prog_3172040.app_users_profile;
+
+REPLACE INTO epus_prog_3172.app_users_list		select * from epus_prog_3172050.app_users_list;
+REPLACE INTO epus_prog_3172.app_users_profile	select * from epus_prog_3172050.app_users_profile;
+
+REPLACE INTO epus_prog_3172.app_users_list		select * from epus_prog_3172060.app_users_list;
+REPLACE INTO epus_prog_3172.app_users_profile	select * from epus_prog_3172060.app_users_profile;
+
+REPLACE INTO epus_prog_3172.app_users_list		select * from epus_prog_3172070.app_users_list;
+REPLACE INTO epus_prog_3172.app_users_profile	select * from epus_prog_3172070.app_users_profile;
+
+REPLACE INTO epus_prog_3172.app_users_list		select * from epus_prog_3172080.app_users_list;
+REPLACE INTO epus_prog_3172.app_users_profile	select * from epus_prog_3172080.app_users_profile;
+
+REPLACE INTO epus_prog_3172.app_users_list		select * from epus_prog_3172090.app_users_list;
+REPLACE INTO epus_prog_3172.app_users_profile	select * from epus_prog_3172090.app_users_profile;
+
+REPLACE INTO epus_prog_3172.app_users_list		select * from epus_prog_3172100.app_users_list;
+REPLACE INTO epus_prog_3172.app_users_profile	select * from epus_prog_3172100.app_users_profile;
+
+
+REPLACE INTO epus_prog_3172.data_keluarga 					select * from epus_prog_3172010.data_keluarga;
+REPLACE INTO epus_prog_3172.data_keluarga_anggota 			select * from epus_prog_3172010.data_keluarga_anggota;
+REPLACE INTO epus_prog_3172.data_keluarga_anggota_profile 	select * from epus_prog_3172010.data_keluarga_anggota_profile;
+REPLACE INTO epus_prog_3172.data_keluarga_kb 				select * from epus_prog_3172010.data_keluarga_kb;
+REPLACE INTO epus_prog_3172.data_keluarga_pembangunan 		select * from epus_prog_3172010.data_keluarga_pembangunan;
+REPLACE INTO epus_prog_3172.data_keluarga_profile 			select * from epus_prog_3172010.data_keluarga_profile;
+
+REPLACE INTO epus_prog_3172.data_keluarga 					select * from epus_prog_3172020.data_keluarga;
+REPLACE INTO epus_prog_3172.data_keluarga_anggota 			select * from epus_prog_3172020.data_keluarga_anggota;
+REPLACE INTO epus_prog_3172.data_keluarga_anggota_profile 	select * from epus_prog_3172020.data_keluarga_anggota_profile;
+REPLACE INTO epus_prog_3172.data_keluarga_kb 				select * from epus_prog_3172020.data_keluarga_kb;
+REPLACE INTO epus_prog_3172.data_keluarga_pembangunan 		select * from epus_prog_3172020.data_keluarga_pembangunan;
+REPLACE INTO epus_prog_3172.data_keluarga_profile 			select * from epus_prog_3172020.data_keluarga_profile;
+
+REPLACE INTO epus_prog_3172.data_keluarga 					select * from epus_prog_3172030.data_keluarga;
+REPLACE INTO epus_prog_3172.data_keluarga_anggota 			select * from epus_prog_3172030.data_keluarga_anggota;
+REPLACE INTO epus_prog_3172.data_keluarga_anggota_profile 	select * from epus_prog_3172030.data_keluarga_anggota_profile;
+REPLACE INTO epus_prog_3172.data_keluarga_kb 				select * from epus_prog_3172030.data_keluarga_kb;
+REPLACE INTO epus_prog_3172.data_keluarga_pembangunan 		select * from epus_prog_3172030.data_keluarga_pembangunan;
+REPLACE INTO epus_prog_3172.data_keluarga_profile 			select * from epus_prog_3172030.data_keluarga_profile;
+
+REPLACE INTO epus_prog_3172.data_keluarga 					select * from epus_prog_3172040.data_keluarga;
+REPLACE INTO epus_prog_3172.data_keluarga_anggota 			select * from epus_prog_3172040.data_keluarga_anggota;
+REPLACE INTO epus_prog_3172.data_keluarga_anggota_profile 	select * from epus_prog_3172040.data_keluarga_anggota_profile;
+REPLACE INTO epus_prog_3172.data_keluarga_kb 				select * from epus_prog_3172040.data_keluarga_kb;
+REPLACE INTO epus_prog_3172.data_keluarga_pembangunan 		select * from epus_prog_3172040.data_keluarga_pembangunan;
+REPLACE INTO epus_prog_3172.data_keluarga_profile 			select * from epus_prog_3172040.data_keluarga_profile;
+
+REPLACE INTO epus_prog_3172.data_keluarga 					select * from epus_prog_3172050.data_keluarga;
+REPLACE INTO epus_prog_3172.data_keluarga_anggota 			select * from epus_prog_3172050.data_keluarga_anggota;
+REPLACE INTO epus_prog_3172.data_keluarga_anggota_profile 	select * from epus_prog_3172050.data_keluarga_anggota_profile;
+REPLACE INTO epus_prog_3172.data_keluarga_kb 				select * from epus_prog_3172050.data_keluarga_kb;
+REPLACE INTO epus_prog_3172.data_keluarga_pembangunan 		select * from epus_prog_3172050.data_keluarga_pembangunan;
+REPLACE INTO epus_prog_3172.data_keluarga_profile 			select * from epus_prog_3172050.data_keluarga_profile;
+
+REPLACE INTO epus_prog_3172.data_keluarga 					select * from epus_prog_3172060.data_keluarga;
+REPLACE INTO epus_prog_3172.data_keluarga_anggota 			select * from epus_prog_3172060.data_keluarga_anggota;
+REPLACE INTO epus_prog_3172.data_keluarga_anggota_profile 	select * from epus_prog_3172060.data_keluarga_anggota_profile;
+REPLACE INTO epus_prog_3172.data_keluarga_kb 				select * from epus_prog_3172060.data_keluarga_kb;
+REPLACE INTO epus_prog_3172.data_keluarga_pembangunan 		select * from epus_prog_3172060.data_keluarga_pembangunan;
+REPLACE INTO epus_prog_3172.data_keluarga_profile 			select * from epus_prog_3172060.data_keluarga_profile;
+
+REPLACE INTO epus_prog_3172.data_keluarga 					select * from epus_prog_3172070.data_keluarga;
+REPLACE INTO epus_prog_3172.data_keluarga_anggota 			select * from epus_prog_3172070.data_keluarga_anggota;
+REPLACE INTO epus_prog_3172.data_keluarga_anggota_profile 	select * from epus_prog_3172070.data_keluarga_anggota_profile;
+REPLACE INTO epus_prog_3172.data_keluarga_kb 				select * from epus_prog_3172070.data_keluarga_kb;
+REPLACE INTO epus_prog_3172.data_keluarga_pembangunan 		select * from epus_prog_3172070.data_keluarga_pembangunan;
+REPLACE INTO epus_prog_3172.data_keluarga_profile 			select * from epus_prog_3172070.data_keluarga_profile;
+
+REPLACE INTO epus_prog_3172.data_keluarga 					select * from epus_prog_3172080.data_keluarga;
+REPLACE INTO epus_prog_3172.data_keluarga_anggota 			select * from epus_prog_3172080.data_keluarga_anggota;
+REPLACE INTO epus_prog_3172.data_keluarga_anggota_profile 	select * from epus_prog_3172080.data_keluarga_anggota_profile;
+REPLACE INTO epus_prog_3172.data_keluarga_kb 				select * from epus_prog_3172080.data_keluarga_kb;
+REPLACE INTO epus_prog_3172.data_keluarga_pembangunan 		select * from epus_prog_3172080.data_keluarga_pembangunan;
+REPLACE INTO epus_prog_3172.data_keluarga_profile 			select * from epus_prog_3172080.data_keluarga_profile;
+
+REPLACE INTO epus_prog_3172.data_keluarga 					select * from epus_prog_3172090.data_keluarga;
+REPLACE INTO epus_prog_3172.data_keluarga_anggota 			select * from epus_prog_3172090.data_keluarga_anggota;
+REPLACE INTO epus_prog_3172.data_keluarga_anggota_profile 	select * from epus_prog_3172090.data_keluarga_anggota_profile;
+REPLACE INTO epus_prog_3172.data_keluarga_kb 				select * from epus_prog_3172090.data_keluarga_kb;
+REPLACE INTO epus_prog_3172.data_keluarga_pembangunan 		select * from epus_prog_3172090.data_keluarga_pembangunan;
+REPLACE INTO epus_prog_3172.data_keluarga_profile 			select * from epus_prog_3172090.data_keluarga_profile;
+
+REPLACE INTO epus_prog_3172.data_keluarga 					select * from epus_prog_3172100.data_keluarga;
+REPLACE INTO epus_prog_3172.data_keluarga_anggota 			select * from epus_prog_3172100.data_keluarga_anggota;
+REPLACE INTO epus_prog_3172.data_keluarga_anggota_profile 	select * from epus_prog_3172100.data_keluarga_anggota_profile;
+REPLACE INTO epus_prog_3172.data_keluarga_kb 				select * from epus_prog_3172100.data_keluarga_kb;
+REPLACE INTO epus_prog_3172.data_keluarga_pembangunan 		select * from epus_prog_3172100.data_keluarga_pembangunan;
+REPLACE INTO epus_prog_3172.data_keluarga_profile 			select * from epus_prog_3172100.data_keluarga_profile;
